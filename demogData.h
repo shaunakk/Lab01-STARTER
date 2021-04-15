44ceeedd7f964155b91b7d3bc2facafdf4ee2643
Shaunak Kale <epicshaunak@gmail.com>
2021-04-14 18:44:55 -0700

    hi

--- demogData.h
+++ demogData.h
@@ -15,23 +15,62 @@ class demogData
 {
 public:
   demogData(string inN, string inS, double in65, double in18, double in5, double totBA, double totHS, int totPop14) : name(inN), state(inS), popOver65(in65), popUnder18(in18), popUnder5(in5), BAup(totBA), HSup(totHS), totalPopulation2014(totPop14){};
-  demogData(string inN, string inS, double in65, double in18, double in5, int totPop14) : name(inN), state(inS), popOver65(in65), popUnder18(in18), popUnder5(in5), totalPopulation2014(totPop14){};
+  demogData(string inN, string inS, double in65, double in18, double in5, int totPop14) : name(inN), state(inS), popOver65(in65), popUnder18(in18), popUnder5(in5), BAup(0), HSup(0), totalPopulation2014(totPop14){};
 
   friend std::ostream &operator<<(std::ostream &out, const demogData &DD);
-  string getState() const { return state; }
-  string getName() const { return name; }
-  int getpopOver65Count() const { return popOver65 / 100 * totalPopulation2014; }
-  int getpopUnder18Count() const { return popUnder18 / 100 * totalPopulation2014; }
-  int getpopUnder5Count() const { return popUnder5 / 100 * totalPopulation2014; }
-  int getBAupCount() const { return BAup / 100 * totalPopulation2014; }
-  int getHSupCount() const { return HSup / 100 * totalPopulation2014; }
-  int getPop() const { return totalPopulation2014; }
+  string getState() const
+  {
+    return state;
+  }
+  string getName() const
+  {
+    return name;
+  }
+  int getpopOver65Count() const
+  {
+    return popOver65 / 100 * totalPopulation2014;
+  }
+  int getpopUnder18Count() const
+  {
+    return popUnder18 / 100 * totalPopulation2014;
+  }
+  int getpopUnder5Count() const
+  {
+    return popUnder5 / 100 * totalPopulation2014;
+  }
+  int getBAupCount() const
+  {
+    return BAup / 100 * totalPopulation2014;
+  }
+  int getHSupCount() const
+  {
+    return HSup / 100 * totalPopulation2014;
+  }
+  int getPop() const
+  {
+    return totalPopulation2014;
+  }
 
-  double getpopOver65() const { return popOver65; }
-  double getpopUnder18() const { return popUnder18; }
-  double getpopUnder5() const { return popUnder5; }
-  double getBAup() const { return BAup; }
-  double getHSup() const { return HSup; }
+  double getpopOver65() const
+  {
+    return popOver65;
+  }
+  double getpopUnder18() const
+  {
+    return popUnder18;
+  }
+  double getpopUnder5() const
+  {
+    return popUnder5;
+  }
+  double getBAup() const
+  {
+    return BAup;
+  }
+  double getHSup() const
+  {
+    return HSup;
+  }
 
 private:
   const string name;
